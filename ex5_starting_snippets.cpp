fbed51f18139ccad85c06c67ecdbd0053353eeea
L Carver <wifihum@gmail.com>
2023-09-25 14:57:04 -0700

    remove namespace bf from ex5_starting__snippets.cpp

--- ex5_starting_snippets.cpp
+++ ex5_starting_snippets.cpp
@@ -1,14 +1,22 @@
-namespace bf {
-    string barkfest[] = {
-        "Bark bark bark! ",
-        "Woooof! ",
-        "Yip! ",
-        "Yap yap! ",
-        "Arf! Arf! ",
-        "Hooowwwwwllll!! ",
-    };
-}
+//------------------------------------------------------------------------------
+// ex5_starting_snippets.cpp
+//------------------------------------------------------------------------------
 
-int highest_bark_index = sizeof(bf::barkfest) / sizeof(string);
+// number of barks will be in range 1-20
+constexpr int BARK_COUNT = 20;
 
+// array of bark sounds
+string barkfest[] = {
+    "Bark bark bark! ",
+    "Woooof! ",
+    "Yip! ",
+    "Yap yap! ",
+    "Arf! Arf! ",
+    "Hooowwwwwllll!! ",
+};
+
+// how many elements are there in barkfest[] array?
+int highest_bark_index = sizeof(barkfest) / sizeof(string);
+
+// seed random number generator with # seconds since 12am on 1/1/1970
 srand((unsigned int)time(0));
